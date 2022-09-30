5415a40a47b73edb5a90ed9bed473f04df85bbd7
Juan Feris <feris.juan@gmail.com>
2022-09-30 16:04:40 -0500

    feat(be): mutations for todos

--- server/index.ts
+++ server/index.ts
@@ -17,6 +17,10 @@ const typeDefs = `
     type Query {
         getTodos: [Todo]!
     }
+    type Mutation {
+        addTodo(text: String!): Todo
+        setDone(id: ID!, done: Boolean!): Todo
+    }
 `
 
 const resolvers = {
@@ -24,9 +28,33 @@ const resolvers = {
         getTodos: () => {
             return todos
         }
+    },
+    Mutation: {
+        addTodo: (_: unknown, { text }: { text: string }) => {
+            const newTodo = {
+                id: String(todos.length + 1),
+                done: false,
+                text
+            }
+            todos.push(newTodo)
+
+            return newTodo
+        },
+        setDone: (_: unknown, { id, done }: { id: string, done: boolean }) => {
+            const todo = todos.find(todo => todo.id === id);
+            if (!todo) throw new Error("Todo not found");
+
+            todo.done = done
+            return todo
+        }
     }
 }
 const server = createServer({
+    cors: {
+        origin: 'http://localhost:3000',
+        credentials: false,
+        methods: ['POST']
+    },
     schema: {
         typeDefs,
         resolvers
