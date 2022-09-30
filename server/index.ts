f5a20e8257f68498ab40ea800fc81d6b3b9405ee
Juan Feris <feris.juan@gmail.com>
2022-09-30 17:47:28 -0500

    feat(fs): add subscriptions

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,16 @@
+import { createPubSub, PubSub } from '@graphql-yoga/node';
 import { createServer } from 'graphql-yoga';
+
+
+const TODOS_CHANNEL = "TODOS_CHANNEL"
+export type PubSubChannels = {
+    TODOS_CHANNEL: [{ todos: Record<string, any>[] }]
+}
+const pubsub = createPubSub<PubSubChannels>()
+
+type GraphQLContext = {
+    pubsub: typeof pubsub
+}
 
 let todos = [
     {
@@ -21,6 +33,9 @@ const typeDefs = `
         addTodo(text: String!): Todo
         setDone(id: ID!, done: Boolean!): Todo
     }
+    type Subscription {
+        todos: [Todo]!
+    }
 `
 
 const resolvers = {
@@ -30,38 +45,52 @@ const resolvers = {
         }
     },
     Mutation: {
-        addTodo: (_: unknown, { text }: { text: string }) => {
+        addTodo: (
+            _: unknown,
+            { text }: { text: string },
+            { pubsub }: { pubsub: PubSub<PubSubChannels> }
+        ) => {
             const newTodo = {
                 id: String(todos.length + 1),
                 done: false,
                 text
             }
             todos.push(newTodo)
-
+            pubsub.publish(TODOS_CHANNEL, { todos })
             return newTodo
         },
-        setDone: (_: unknown, { id, done }: { id: string, done: boolean }) => {
-            console.log('🚀🚀🚀 ~ file: index.ts ~ line 44 ~ done', done)
+        setDone: (
+            _: unknown,
+            { id, done }: { id: string, done: boolean },
+            { pubsub }: { pubsub: PubSub<PubSubChannels> }
+        ) => {
             const todo = todos.find(todo => {
                 return todo.id === id
             })
-            console.log('🚀🚀🚀 ~ file: index.ts ~ line 45 ~ todo', todo)
             if (!todo) throw new Error("Todo not found");
 
             todo.done = done
+            pubsub.publish(TODOS_CHANNEL, { todos })
             return todo
+        }
+    },
+    Subscription: {
+        todos: {
+            subscribe: (_: unknown, args: {}, context: GraphQLContext) => context.pubsub.subscribe(TODOS_CHANNEL)
         }
     }
 }
+
 const server = createServer({
     cors: {
         origin: 'http://localhost:3000',
         credentials: false,
         methods: ['POST']
     },
+    context: { pubsub },
     schema: {
+        resolvers,
         typeDefs,
-        resolvers
     }
 })
 
